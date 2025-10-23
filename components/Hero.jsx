c14a5c8a1b4063dc51d5fe1eb8a56d37f1f8833f
Zohaib <ranjha5054@gmail.com>
2025-10-23 17:59:19 +0500

    make a gap between the hero left and hero right because button was not clicking well on mobile

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <section id="home" className="h-auto md:h-[90.3vh]">
       <div className="fixed inset-0 z-0" style={{ backgroundImage: `linear-gradient(to right, #262626 1px, transparent 1px),linear-gradient(to bottom, #262626 1px, transparent 1px)`, backgroundSize: "20px 20px"}}></div>
-      <div className="max-width relative z-10 flex flex-col md:flex-row gap-0 h-auto md:h-full">
+      <div className="max-width relative z-10 flex flex-col md:flex-row gap-3 md:gap-0 h-auto md:h-full">
         <HeroLeft />
         <HeroRight />
       </div>
--- components/HeroRight.jsx
+++ components/HeroRight.jsx
@@ -4,7 +4,7 @@ import SocialIcons from "./SocialIcons";
 
 export default function HeroRight() {
   return (
-    <div className="relative w-full md:w-[40%] h-80 md:h-full">
+    <div className="relative w-full md:w-[40%] h-80 md:h-full mt-3 md:mt-0">
         <Image src={zohaib} alt="Zohaib Ranjha" className="absolute bottom-0 drop-shadow-[0_4px_8px_#31F900] transition md:saturate-150" width={600} height={600} />
         <SocialIcons />
     </div>
